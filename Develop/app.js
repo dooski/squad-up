52196e22dca67bac2fabf1152ed92df4e0529e28
Dooski <dmckeon495@gmail.com>
2020-05-03 17:56:33 -0400

    final push

--- Develop/app.js
+++ Develop/app.js
@@ -3,8 +3,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
-const fsPromises = require("fs").promises;
+const fs = require("fs").promises;
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -14,7 +13,6 @@ const render = require("./lib/htmlRenderer");
 //array for squad
 const squadArray = []
 
-squadStart()
 //squad assembly with inquirer; starts with beginBuilder that gets manager info
 function squadStart() {
     return inquirer.prompt([
@@ -51,7 +49,7 @@ function squadStart() {
         });
 };
 
-// 
+//function that loops back to add either intern or engineer
 function addSquad() {
     return inquirer.prompt([
         {
@@ -66,11 +64,11 @@ function addSquad() {
                 addEngineer();
             } else if (answers.memberType === "Intern") {
                 addIntern();
-            } else { squadComplete(); }
+            } else { squadComplete(squadArray); }
         }
         )
 };
-
+//function to add new engineers
 function addEngineer() {
     return inquirer.prompt([
         {
@@ -106,6 +104,7 @@ function addEngineer() {
         });
 };
 
+//function add interns
 function addIntern() {
     return inquirer.prompt([
         {
@@ -141,28 +140,19 @@ function addIntern() {
         });
 };
 
-function squadComplete() {
+function squadComplete(squadArray) {
+    fs.writeFile(outputPath, render(squadArray));
     console.log(squadArray)
 }
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+async function init() {
+    try {
+        squadStart();
+    } catch (err) {
+        console.log(err);
+    };
+};
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+init();
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# squad-up
+# SQUAD UP
+### An app for organizing your squad of engineers and interns
+---
+1. Use
+SQUAD UP is used to create a simple HTML page displaying your squad's information, including all engineers and interns and appropriate contact information, conveniently in one page.
+
+2. Technologies used
+SQUAD UP uses node.js, jest, inquirer, Bootstrap and HTML.
+
+3. Future development
+Custom employee classes to allow more flexibility.
+
+4. Pictures
+![](Develop/img.PNG)
+
+5. GitHub Link
+https://github.com/dooski/squad-up
